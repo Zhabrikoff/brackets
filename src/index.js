df25c2b7dc87c8640e5eaa0ee82219c25935f17d
Yauheni Zhabrykau <ezhabrikov@gmail.com>
2021-02-17 11:27:32 +0300

    feat: implement function check(str, bracketsConfig)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let configArr = [];
+  bracketsConfig.map((item) => {
+    configArr.push(item.join(''));
+  });
+
+  let count = 0;
+  let strLenght = str.length / 2;
+  while (count < strLenght) {
+    configArr.map((item) => {
+      str = str.replace(item, '')
+    });
+    count++;
+  }
+
+  return str.length === 0 ? true : false;
 }
